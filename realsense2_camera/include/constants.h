36ddd771eb6a2ec4cd7edd7dad8c52f0addd4766
Nir Azkiel <nir.azkiel@intel.com>
2025-04-10 10:41:21 +0300

    update version to 4.56.4

--- realsense2_camera/include/constants.h
+++ realsense2_camera/include/constants.h
@@ -18,8 +18,8 @@
 #include "ros_node_base.h"
 
 #define REALSENSE_ROS_MAJOR_VERSION    4
-#define REALSENSE_ROS_MINOR_VERSION    55
-#define REALSENSE_ROS_PATCH_VERSION    1
+#define REALSENSE_ROS_MINOR_VERSION    56
+#define REALSENSE_ROS_PATCH_VERSION    4
 
 #define STRINGIFY(arg) #arg
 #define VAR_ARG_STRING(arg) STRINGIFY(arg)
